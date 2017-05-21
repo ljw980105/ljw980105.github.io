d50092927799243848881a0248e4c889316fc94f
Jing Wei Li <ljw9801055@gmail.com>
2017-05-21 13:42:07 -0400

    Version 2.0

--- JS/main.js
+++ JS/main.js
@@ -24,32 +24,16 @@ Attempt to zoom while hovering
  */
 function revealSM() {
      var $sm = $('#social_media a');
-    // $sm.hover(function() {
-    //     $(this).css("cursor", "pointer");
-    //     $(this).toggle({
-    //         effect: "scale",
-    //         percent: "110%"
-    //     },200);
-    // }, function() {
-    //     $(this).toggle({
-    //         effect: "scale",
-    //         percent: "100%"
-    //     },200);
-    // });
-
-    // $sm.hide();
-    // // $sm.slideToggle(500);
-    // $sm.toggle("slide");
-
 }
 
 // primary syntax of jQuery
 $(document).ready(function () { // wait until webpage is ready
     revealSM();
-    var $contents = $('#contents');
+    var $contents = $('#nav');
     $contents.hide();
     //$contents.fadeIn(1000);
-    $contents.slideToggle(1500);
+    $contents.slideToggle(500);
+    
 
 });
 //main();
