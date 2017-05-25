7417366f6fdc347b1ce2dd8fb46227a21603169c
Jing Wei Li <ljw9801055@gmail.com>
2017-05-25 13:56:49 -0400

    Added Featured Projects in Index

--- JS/main.js
+++ JS/main.js
@@ -59,6 +59,13 @@ $(document).ready(function () { // wait until webpage is ready
     //$contents.fadeIn(1000);
 
     // var $sm = $('#sm1');
+    var $proj = $("#contents a");
+    $proj.mouseenter(function () {
+        $(this).fadeTo("fast",0.8)
+    });
+    $proj.mouseleave(function () {
+        $(this).fadeTo("fast",1)
+    });
 
     opacityFx(1);
     opacityFx(2);
