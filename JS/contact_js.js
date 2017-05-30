8eb2d30b0931a936941e1c3a8c48b9f41bfab3b3
Jing Wei Li <ljw9801055@gmail.com>
2017-05-30 14:24:14 -0400

    Scroll to Reveal Effect Applied to Contacts

--- JS/contact_js.js
+++ JS/contact_js.js
@@ -21,6 +21,30 @@ function opacityFx(num){
 }
 
 $(document).ready(function () {
+    var num2 = $('#email').offset().top;
+    var num3 = $('#social_media').offset().top;
+    var num4 = $('#footer').offset().top;
+    // alert(num2);
+    $('#phone').hide();
+    $('#email').hide();
+    $('#social_media').hide();
+    $("#footer").hide();
+
+    $('#phone').fadeIn(1500);
+    $(document).scroll(function () {
+        var curr = $(this).scrollTop();
+        if (curr > num2-700) {
+            $('#email').fadeIn(2000);
+        }
+        if (curr > num3-700) {
+            $('#social_media').fadeIn(2000);
+        }
+        if (curr > num4-900) {
+            $('#footer').fadeIn(2000);
+        }
+    });
+
+
     opacityFx(1);
     opacityFx(2);
     opacityFx(3);
