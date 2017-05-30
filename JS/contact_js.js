477239161480d06618f517fef7f372edebe03937
Jing Wei Li <ljw9801055@gmail.com>
2017-05-30 14:02:14 -0400

    Finished Contact Page

---
+++ JS/contact_js.js
@@ -0,0 +1,29 @@
+function opacityFx(num){
+    var $sm = $("#sm" + num.toString());
+    $sm.mouseenter(function() {
+        //$(this).fadeTo("fast",0.7);
+        for (var i = 1; i < 6; i ++){
+            if (i !== num){
+                $media = $("#sm" + i.toString());
+                $media.fadeTo("fast",0.4)
+            }
+        }
+    });
+    $sm.mouseleave(function() {
+        //$(this).fadeTo("fast",1);
+        for (var i = 1; i < 6; i ++){
+            if (i !== num){
+                $media = $("#sm" + i.toString());
+                $media.fadeTo("fast",1)
+            }
+        }
+    });
+}
+
+$(document).ready(function () {
+    opacityFx(1);
+    opacityFx(2);
+    opacityFx(3);
+    opacityFx(4);
+    opacityFx(5);
+});
