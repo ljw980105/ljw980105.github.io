38f6abf76e34160cb1a1f9ea8ee7db40affa1b74
Jing Wei Li <ljw9801055@gmail.com>
2017-07-08 20:29:56 -0400

    Updated Projects Page with 2048

--- JS/proj_js.js
+++ JS/proj_js.js
@@ -14,12 +14,15 @@ $(document).ready(function () {
     $typ = $("#typography");
     $item1 = $("#col1_item1");
     $item2 = $("#col2_item1");
+    $item3 = $("#col1_item2");
 
     $typ.fadeTo(1,0.001);
     $item1.fadeTo(1,0.001);
     $item2.fadeTo(1,0.001);
+    $item3.fadeTo(1,0.001);
 
     var num1 = $item1.offset().top;
+    var num2 = $item3.offset().top;
 
     setTimeout(function () {
         $typ.fadeTo(1000,1);
@@ -30,6 +33,9 @@ $(document).ready(function () {
         if (curr > num1 - 600){
             orderedFade($item1,1);
             orderedFade($item2,2);
+        }
+        if (curr > num2 - 600){
+            orderedFade($item3,1);
         }
     });
 });
