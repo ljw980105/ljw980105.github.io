259f029faa5d5486b21dfa595f325d5ec5c44811
Jing Wei Li <ljw9801055@gmail.com>
2017-07-22 16:56:18 -0400

    Projcts and Bio Now Responsive

--- JS/common_jquery.js
+++ JS/common_jquery.js
@@ -2,4 +2,12 @@ $(document).ready(function () { // wait until webpage is ready
     var $nav = $('#nav');
     $nav.hide();
     $nav.slideToggle(500);
+
+    //CODE FOR SLIDING MOBILE NAV BAR
+    var $mobile_nav = $("#mobile_nav");
+    var $navicon = $("#navicon");
+    $mobile_nav.hide();
+    $navicon.click(function () {
+        $mobile_nav.slideToggle();
+    });
 });
