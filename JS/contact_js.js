ef8319f206c3fb05661770b29488e39992982896
Jing Wei Li <ljw9801055@gmail.com>
2017-09-01 20:31:38 -0400

    Fixed JS in Contacts Including Mobile Version

--- JS/contact_js.js
+++ JS/contact_js.js
@@ -1,20 +1,20 @@
-function opacityFx(num){
+function opacityFx(num) {
     var $sm = $("#sm" + num.toString());
-    $sm.mouseenter(function() {
+    $sm.mouseenter(function () {
         //$(this).fadeTo("fast",0.7);
-        for (var i = 1; i < 6; i ++){
-            if (i !== num){
+        for (var i = 1; i < 6; i++) {
+            if (i !== num) {
                 $media = $("#sm" + i.toString());
-                $media.fadeTo("fast",0.4)
+                $media.fadeTo("fast", 0.4)
             }
         }
     });
-    $sm.mouseleave(function() {
+    $sm.mouseleave(function () {
         //$(this).fadeTo("fast",1);
-        for (var i = 1; i < 6; i ++){
-            if (i !== num){
+        for (var i = 1; i < 6; i++) {
+            if (i !== num) {
                 $media = $("#sm" + i.toString());
-                $media.fadeTo("fast",1)
+                $media.fadeTo("fast", 1)
             }
         }
     });
@@ -25,29 +25,60 @@ $(document).ready(function () {
     var num1 = $('#p_icon').offset().top;
     var num2 = $('#e_icon').offset().top;
     var num3 = $('#sub_sm').offset().top;
-    var num4 = $('#f00ter').offset().top;
+
+    // var num4 = $('#f00ter').offset().top;
+    var num5 = $('#phone_mobile').offset().top;
+    var num6 = $('#email_mobile').offset().top;
+
     // alert(num2);
     //$('#phone').hide();
-    $('#p_icon').fadeTo(1,0.001);
-    $('#e_icon').fadeTo(1,0.001);
-    $('#sub_sm').fadeTo(1,0.001);
-    $("#f00ter").fadeTo(1,0.001);
+    $('#title_').fadeTo(1, 0.001);
+    $('#p_icon').fadeTo(1, 0.001);
+    $('#e_icon').fadeTo(1, 0.001);
+    $('#sub_sm').fadeTo(1, 0.001);
+    // $("#f00ter").fadeTo(1,0.001);
 
     //$('#phone').fadeIn(1500);
+    setTimeout(function () {
+        $("#title_").fadeTo(1000, 1);
+    }, 500);
+
+
+    $("#email_mobile, #phone_mobile").fadeTo(1, 0.001);
+
+
     $(document).scroll(function () {
         var curr = $(this).scrollTop();
-        if (curr > num1-700){
-            $('#p_icon').fadeTo(2000,1);
+
+        if ($(window).width() > 550){
+            // for desktop
+            if (curr > num1 - 700) {
+                $('#p_icon').fadeTo(2000, 1);
+            }
+            if (curr > num2 - 600) {
+                $('#e_icon').fadeTo(2000, 1);
+            }
+            if (curr > num3 - 700) {
+                $('#sub_sm').fadeTo(2000, 1);
+            }
         }
-        if (curr > num2-600) {
-            $('#e_icon').fadeTo(2000,1);
+
+        // if (curr > num4 - 900) {
+        //     // $('#f00ter').fadeTo(2000,1);
+        // }
+        if ($(window).width() <= 550){
+            //for mobile
+            if (curr > num5 - 300) {
+                $("#phone_mobile").fadeTo(1500, 1);
+            }
+            if (curr > num6 - 300) {
+                $("#email_mobile").fadeTo(1500, 1);
+            }
+            if (curr > num3 - 300) {
+                $('#sub_sm').fadeTo(1500, 1);
+            }
         }
-        if (curr > num3-700) {
-            $('#sub_sm').fadeTo(2000,1);
-        }
-        if (curr > num4-900) {
-            $('#f00ter').fadeTo(2000,1);
-        }
+
     });
 
     opacityFx(1);
