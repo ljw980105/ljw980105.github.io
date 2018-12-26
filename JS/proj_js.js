c1a6470b09adea4d61dbd3b7337873dda9587ec9
ljw980105 <ljw9801055@gmail.com>
2018-12-26 14:49:39 -0500

    Added Fall 2018 Projects

--- JS/proj_js.js
+++ JS/proj_js.js
@@ -3,7 +3,7 @@ Pass in the objects to fade in, and their order.
  */
 function orderedFade(obj,order){
     order--;
-    var delay = order * 500;
+    let delay = order * 500;
 
     setTimeout(function () {
         obj.fadeTo(1000,1);
@@ -18,6 +18,8 @@ $(document).ready(function () {
     $item4 = $("#col2_item2");
     $item5 = $("#col1_item3");
     $item6 = $("#col2_item3");
+    $item7 = $("#col1_item4");
+    $item8 = $("#col2_item4");
 
     $typ.fadeTo(1,0.001);
     $item1.fadeTo(1,0.001);
@@ -26,17 +28,20 @@ $(document).ready(function () {
     $item4.fadeTo(1,0.001);
     $item5.fadeTo(1,0.001);
     $item6.fadeTo(1,0.001);
+    $item7.fadeTo(1,0.001);
+    $item8.fadeTo(1,0.001);
 
-    var num1 = $item1.offset().top;
-    var num2 = $item3.offset().top;
-    var num3 = $item5.offset().top;
+    let num1 = $item1.offset().top;
+    let num2 = $item3.offset().top;
+    let num3 = $item5.offset().top;
+    let num4 = $item7.offset().top;
 
     setTimeout(function () {
         $typ.fadeTo(1000,1);
     },500);
 
     $(document).scroll(function () {
-        var curr = $(this).scrollTop();
+        let curr = $(this).scrollTop();
         if (curr > num1 - 600){
             orderedFade($item1,1);
             orderedFade($item2,2);
@@ -48,6 +53,10 @@ $(document).ready(function () {
         if (curr > num3 - 600){
             orderedFade($item5,1);
             orderedFade($item6,2);
+        }
+        if (curr > num4 - 600) {
+            orderedFade($item7,1);
+            orderedFade($item8,2);
         }
     });
 });
